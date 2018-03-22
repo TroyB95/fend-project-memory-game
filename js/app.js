b9fab8aa5d178db5d63ee35a651fefeaa077ef1b
TroyB <troy_budd95@msn.com>
2018-03-22 09:12:21 +0000

    Added create grrid function which shuffles cards and adds them to grid

--- js/app.js
+++ js/app.js
@@ -17,3 +17,36 @@ function showCard() {
 	this.classList.toggle('remove-events')
 }
 
+// Select deck which contains all the cards
+const deck = document.querySelector(".deck");
+
+createGrid();
+
+function createGrid(){
+
+// A variable which stores the shuffled cards
+	let shuffled = shuffle(cards);
+
+// A loop which adds the shuffled cards into the deck
+	for(var i = 0; i < shuffled.length; i++){
+		[].forEach.call(shuffled, function(card){
+			deck.appendChild(card);
+		});
+		 cards[i].classList.remove("show", "open", "match");
+	}
+};
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
