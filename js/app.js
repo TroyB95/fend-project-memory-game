79e085fcc21cccbf389d0b93c3cfb4e65a96c2b3
TroyB <troy_budd95@msn.com>
2018-03-22 09:11:12 +0000

    Added show card function and loop which loops all cards

--- js/app.js
+++ js/app.js
@@ -2,4 +2,18 @@
 let card = document.getElementsByClassName('card');
 let cards = [...card];
 
+// Array which will compare to see if matching
+let flippedCards = [];
+
+// Loop through cards to display
+for (var i = 0; i < cards.length; i++) {
+	cards[i].addEventListener('click', showCard)
+}
+
+// Toggle open and show class when card is clicked
+function showCard() {
+	this.classList.toggle('open');
+	this.classList.toggle('show');
+	this.classList.toggle('remove-events')
+}
 
